2d1ac3ea3bc4db30a34c4606e7d48490bb44d28c
uppalatirumalesh <uppalatirumalesh@gmail.com>
2023-05-28 09:27:28 +0530

    third commit

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-const brandCheckboxes = document.querySelectorAll('.checkbox-value');
-const colorCheckboxes = document.querySelectorAll('.SelectedColor');
-const priceCheckboxes = document.querySelectorAll('.SelectedPrice');
-const images = document.querySelectorAll('.images');
+const brandCheckboxes = document.querySelectorAll(".checkbox-value");
+const colorCheckboxes = document.querySelectorAll(".SelectedColor");
+const priceCheckboxes = document.querySelectorAll(".SelectedPrice");
+const images = document.querySelectorAll(".images");
+const search = document.querySelector(".search");
+
+search.addEventListener("keyup", searchBrand);
+
+function searchBrand() {
+  images.forEach((image) => {
+    const brand = image.id;
+    const color = image.getAttribute("data-color");
+    let x = document.getElementById("search-box");
+    if (brand.toLowerCase().includes(x.value.toLowerCase())) {
+      image.style.display = "block";
+    } else if (color.toLowerCase().includes(x.value.toLowerCase())) {
+      image.style.display = "block";
+    } else {
+      image.style.display = "none";
+    }
+  });
+}
 
 brandCheckboxes.forEach((checkbox) => {
-  checkbox.addEventListener('change', filterImages);
+  checkbox.addEventListener("change", filterImages);
 });
 
 colorCheckboxes.forEach((checkbox) => {
-  checkbox.addEventListener('change', filterImages);
+  checkbox.addEventListener("change", filterImages);
 });
 
 priceCheckboxes.forEach((checkbox) => {
-  checkbox.addEventListener('change', filterImages);
+  checkbox.addEventListener("change", filterImages);
 });
 
 function filterImages() {
@@ -30,17 +48,21 @@ function filterImages() {
 
   images.forEach((image) => {
     const brand = image.id;
-    const color = image.getAttribute('data-color');
-    const priceRange = image.getAttribute('data-price');
+    const color = image.getAttribute("data-color");
+    const priceRange = image.getAttribute("data-price");
 
-    const brandFilter = selectedBrands.length === 0 || selectedBrands.includes(brand);
-    const colorFilter = selectedColors.length === 0 || selectedColors.includes(color);
-    const priceFilter = selectedPriceRanges.length === 0 || selectedPriceRanges.includes(priceRange);
+    const brandFilter =
+      selectedBrands.length === 0 || selectedBrands.includes(brand);
+    const colorFilter =
+      selectedColors.length === 0 || selectedColors.includes(color);
+    const priceFilter =
+      selectedPriceRanges.length === 0 ||
+      selectedPriceRanges.includes(priceRange);
 
     if (brandFilter && colorFilter && priceFilter) {
-      image.style.display = 'block';
+      image.style.display = "block";
     } else {
-      image.style.display = 'none';
+      image.style.display = "none";
     }
   });
 }
